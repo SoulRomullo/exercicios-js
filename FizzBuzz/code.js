4dc9aee6e87d36bb4adc9f632dd3a838ee9794eb
SoulRomullo <soulromullo@gmail.com>
2022-10-31 09:06:04 -0300

    feat: exercício feito fizzbuzz

--- FizzBuzz/code.js
+++ FizzBuzz/code.js
@@ -1,5 +1,33 @@
 function fizzbuzz() {
     // code
+    let hash = "#";
+    for(cont = 14; cont >= hash.length; cont--) {
+        console.log(hash);
+        hash += "#";
+    }
+
+    let num = 0;
+    let hashNew = "";
+    while(num <= 7) {
+        console.log(hashNew);
+        hashNew += "#";
+        num ++; 
+    }
+
+    console.log("")
+
+    hashNewDo = "#";
+    numNew = 0;
+    do { 
+        console.log(hashNewDo)
+        hashNewDo += "#";
+        numNew ++;
+    }while(numNew <= 6) {
+       
+    }
+   
 }
 
 module.exports = fizzbuzz
+
+fizzbuzz()
