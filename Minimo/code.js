25f9101d13a27406f2b364afa2bb8aaaefb1b4c4
SoulRomullo <soulromullo@gmail.com>
2022-10-31 18:25:49 -0300

    feat: concluido o exercício

--- Minimo/code.js
+++ Minimo/code.js
@@ -1,5 +1,12 @@
-function minimo() {
+function minimo(num1, num2) {
     // code
+    if(num1 < num2) {
+        console.log("O número menor é " + num1);
+    }else {
+        console.log("O número menor é " + num2);
+    }
 }
+
+minimo(1, 0)
 
 module.exports = minimo
