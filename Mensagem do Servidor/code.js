cac711157236d0376f3d6781db7004531e6bc889
SoulRomullo <soulromullo@gmail.com>
2022-11-30 18:15:39 -0300

    feat: exercicio mensagem do servidor feito

--- Mensagem do Servidor/code.js
+++ Mensagem do Servidor/code.js
@@ -2,15 +2,19 @@ const Helper = require("./helper.js")
 
 function code(message) {
     // code
-
     const user = Helper.getUsername();
     const notifications = Helper.getNotifications();
 
-    
-    return 
-    
+    let userNotifications = message.replaceAll("{{user_name}}", user)
+    userNotifications = userNotifications.replaceAll("{{notifications}}", notifications)
+
+    return userNotifications
 }
 
-code("Olá {{user_name}}")
+
+// console.log(code("Olá {{user_name}}"))
+// console.log(code("Você tem {{notifications}} notificações"))
+// console.log(code("{{user_name}} - {{notifications}}"))
+// console.log(code("{{user_name}}, {{notifications}}: {{user_name}}"))
 
 module.exports = code
